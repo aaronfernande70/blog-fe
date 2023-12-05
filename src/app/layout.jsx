18e354596120fa18eb3dd885c65fa14dee5c8037
aaronfernande70 <aaronfernandes53@gmail.com>
2023-12-05 21:18:07 +0530

    mobile app capable

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Providers } from '@/provider'
 import Header from './components/organisms/Header'
 import './globals.css'
+import Head from 'next/head'
 
 
 export const metadata = {
@@ -10,16 +11,26 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <Providers>
 
     <html lang="en">
+      <Head>
+        <title>
+          blog site
+        </title>
+        {/* <meta
+          name="description"
+          content="Check out iPhone 12 XR Pro and iPhone 12 Pro Max. Visit your local store and for expert advice."
+          key="desc"
+        /> */}
+      <meta name="mobile-web-app-capable" content="yes"></meta>
+
+      </Head>
       <body className="font-serif">
         <Header/>
         {children}
         {/* <Footer/> */}
       </body>
     </html>
-    </Providers>
 
   )
 }
