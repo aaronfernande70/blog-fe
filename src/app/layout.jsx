44843c3583acfad81550e0c73411d53fa2038f16
aaronfernande70 <aaronfernandes53@gmail.com>
2023-12-06 09:56:50 +0530

    c

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <link rel="manifest" href="/manifest.json" />
         <meta name="mobile-web-app-capable" content="yes"></meta>
         <meta name="apple-mobile-web-app-capable" content="yes"></meta>
         <meta name="mobile-web-app-capable" content="yes"></meta>
